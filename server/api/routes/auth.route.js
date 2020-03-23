bba8ed6f7b788d1807fc4e52ea2820e5fcffae41
Natnael Abrham <natnael08@protonmain.com>
2020-03-24 02:31:09 +0300

    + login controller

--- server/api/routes/auth.route.js
+++ server/api/routes/auth.route.js
@@ -2,10 +2,16 @@ const router = require("express").Router();
 
 // controller files
 const register = require("../controllers/register.auth"); // REGISTER/SIGN UP CONTROLLER
+const login = require("../controllers/login.auth"); // REGISTER/SIGN UP CONTROLLER
 
 // @route       POST /api/auth/register
 // @desc        register new user
 // @access      public
 router.post("/register", register);
+
+// @route       POST /api/auth/register
+// @desc        register new user
+// @access      public
+router.post("/login", login);
 
 module.exports = router;
