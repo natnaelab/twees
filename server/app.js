48c05345809383c80621759dd8ffff02ed502caf
Natnael Abrham <natnael08@protonmain.com>
2020-03-24 01:43:23 +0300

    linked auth route

--- server/app.js
+++ server/app.js
@@ -13,4 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(compression());
 
+// routes
+const auth = require("./api/routes/auth.route");
+
+app.use("/api/auth", auth);
+
 module.exports = app;
