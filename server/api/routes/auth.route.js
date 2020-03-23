990c04630fa49acaadd1995c4ff826031123d6fe
Natnael Abrham <natnael08@protonmain.com>
2020-03-24 02:54:21 +0300

    added login & session route controllers

--- server/api/routes/auth.route.js
+++ server/api/routes/auth.route.js
@@ -2,16 +2,22 @@ const router = require("express").Router();
 
 // controller files
 const register = require("../controllers/register.auth"); // REGISTER/SIGN UP CONTROLLER
-const login = require("../controllers/login.auth"); // REGISTER/SIGN UP CONTROLLER
+const login = require("../controllers/login.auth"); // LOGIN CONTROLLER
+const session = require("../controllers/session.auth"); // LOGIN CONTROLLER
 
 // @route       POST /api/auth/register
 // @desc        register new user
 // @access      public
 router.post("/register", register);
 
-// @route       POST /api/auth/register
-// @desc        register new user
+// @route       POST /api/auth/login
+// @desc        login user
 // @access      public
 router.post("/login", login);
+
+// @route       POST /api/auth/register
+// @desc        register new user
+// @access      private
+router.post("/session", session);
 
 module.exports = router;
