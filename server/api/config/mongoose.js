0d12aaf8cf65a8f18df1d55d9a647ce751f05fa7
Natnael Abrham <natnael08@protonmain.com>
2020-03-24 01:03:28 +0300

    🔧 minor fix

--- server/api/config/mongoose.js
+++ server/api/config/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-module.exports = mongoose.connect(process.env.mongooseURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+module.exports = mongoose
+  .connect(process.env.mongooseURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.info("mongoDB has been connected!"));
