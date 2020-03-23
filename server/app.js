3920b499f26de7fbca8a3c6a571aa67ae160ffd6
Natnael Abrham <natnael08@protonmain.com>
2020-03-24 02:57:09 +0300

    added error handler

--- server/app.js
+++ server/app.js
@@ -19,4 +19,20 @@ const auth = require("./api/routes/auth.route");
 
 app.use("/api/auth", auth);
 
+// Error handling
+app.use((req, res, next) => {
+  const error = new Error("not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
 module.exports = app;
