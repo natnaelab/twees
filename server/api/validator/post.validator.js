abbd6b82f2f7ec0ba3b0aa55f7a54464df8e1632
Natnael Abrham <natnael08@protonmain.com>
2020-03-26 03:00:59 +0300

    post type check validator

--- server/api/validator/post.validator.js
+++ server/api/validator/post.validator.js
@@ -6,20 +6,38 @@ module.exports = {
   /**
    * post validator
    * post-body (text)
+   * captions (pic/vid)
    */
   validatePostInput: data => {
     let errors = {};
 
-    data.text = !isEmpty(data.username) ? data.username : "";
+    if (data.text) data.text = !isEmpty(data.text) ? data.text : "";
+    if (data.pic)
+      data.pic.caption = !isEmpty(data.pic.caption) ? data.pic.caption : "";
+    if (data.vid)
+      data.vid.caption = !isEmpty(data.vid.caption) ? data.vid.caption : "";
 
-    // check post text or caption chars length
-    !validator.isLength(data.text || data.pic.caption || data.vid.caption, {
-      max: 700
-    }) && (errors.text = "maximum character limit");
+    // check text chars length
+    data.text &&
+      !validator.isLength(data.text, {
+        max: 400
+      }) &&
+      (errors.text = "you can't post more than 400 characters");
 
-    // check if post text or caption field is empty
-    validator.isEmpty(data.text || data.pic.caption || data.vid.caption) &&
-      (errors.text = "required field!");
+    // check if text field is empty
+    data.text &&
+      validator.isEmpty(data.text) &&
+      (errors.text = "text field is required");
+
+    data.pic &&
+      !isEmpty(data.pic.caption) &&
+      !validator.isLength(data.pic.caption, { max: 400 }) &&
+      (errors.pic = "you can't use more than 400 characters");
+
+    data.vid &&
+      !isEmpty(data.vid.caption) &&
+      !validator.isLength(data.vid.caption) &&
+      (errors.vid = "you can't use more than 400 characters");
 
     // return error if the form is invalid
     return {
