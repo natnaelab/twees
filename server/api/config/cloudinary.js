cadc64b3c9a7880135fe2a8d9b38417b49401b89
Natnael Abrham <natnael08@protonmain.com>
2020-03-28 19:19:36 +0300

    + cloudinary api config

--- server/api/config/cloudinary.js
+++ server/api/config/cloudinary.js
@@ -1,5 +1,17 @@
 const cloudinary = require("cloudinary").v2;
 
+const {
+  cloudinary_cloud_name,
+  cloudinary_api_key,
+  cloudinary_api_secret
+} = process.env;
+
+cloudinary.config({
+  cloud_name: cloudinary_cloud_name,
+  api_key: cloudinary_api_key,
+  api_secret: cloudinary_api_secret
+});
+
 module.exports = file => {
   cloudinary.uploader.upload(
     file => result => {
