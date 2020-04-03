d38cd4ce1813176e99a11493eac7304f3124cf33
Natnael Abrham <natnael08@protonmain.com>
2020-04-03 08:43:15 +0300

    new post controller

---
+++ server/api/controllers/new.post.js
@@ -0,0 +1,84 @@
+const { validatePostInput } = require("../validator/post.validator");
+const fs = require("fs");
+const Post = require("../models/post.model");
+const isEmpty = require("../utils/isEmpty");
+const cloudUpload = require("../config/cloudinary");
+
+module.exports = (req, res) => {
+  let post = {};
+  Object.assign(post, req.body);
+
+  const { errors, isValid } = validatePostInput(req.body);
+
+  if (isEmpty(post.text) && isEmpty(req.file))
+    res.status(400).json({ status: "failed", message: "empty post" });
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  if (!req.file && post.text) {
+    post.type = "text";
+    return new Post(post)
+      .save()
+      .then(resp => {
+        res.json({ status: "success" });
+      })
+      .catch(err => {
+        res.status(400).json({ status: "error", message: err.message });
+      });
+  } else if (req.file) {
+    let mimetype = req.file.mimetype;
+    if (mimetype.includes("image")) {
+      post.type = "pic";
+      let pic = {};
+      if (post.caption) {
+        pic.caption = post.caption;
+        delete post.caption;
+      }
+      cloudUpload(req.file.path, req.user.username, "image")
+        .then(resp => {
+          fs.unlinkSync(req.file.path);
+          pic.url = resp.url;
+          return new Post(post)
+            .save()
+            .then(_ => {
+              res.json({ status: "success" });
+            })
+            .catch(err => {
+              res.status(400).json({ status: "error", message: err.message });
+            });
+        })
+        .catch(err => {
+          if (fs.existsSync(req.file.path)) fs.unlinkSync(req.file.path);
+          res.status(400).json(err);
+        });
+      post.pic = pic;
+    } else if (mimetype.includes("video")) {
+      post.type = "vid";
+      let vid = {};
+      if (post.caption) {
+        vid.caption = post.caption;
+        delete post.caption;
+      }
+      cloudUpload(req.file.path, req.user.username, "video")
+        .then(resp => {
+          fs.unlinkSync(req.file.path);
+          vid.url = resp.url;
+          res.json(post);
+        })
+        .catch(err => {
+          if (fs.existsSync(req.file.path)) fs.unlinkSync(req.file.path);
+          return new Post(post)
+            .save()
+            .then(_ => {
+              res.json({ status: "success" });
+            })
+            .catch(err => {
+              res.status(400).json({ status: "error", message: err.message });
+            });
+        });
+      post.vid = vid;
+    }
+  }
+};
