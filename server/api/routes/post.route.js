1c6f672ae53b0bb1839b7c87dcfa44106cf7a406
Natnael Abrham <natnael08@protonmain.com>
2020-04-03 08:44:38 +0300

    multer setup

--- server/api/routes/post.route.js
+++ server/api/routes/post.route.js
@@ -1,14 +1,16 @@
 const router = require("express").Router();
 const privateRoute = require("../middlewares/private_route");
+const upload = require("../config/multer");
 
 // controller files
 const newPost = require("../controllers/new.post"); // new post
 // delete post
 // like post
+// edit post
 
 // @route       POST /api/post
 // @desc        add new post
 // @access      private
-router.post("/", privateRoute, newPost);
+router.post("/", privateRoute, upload.single("upload"), newPost);
 
 module.exports = router;
