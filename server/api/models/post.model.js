970490383bd98a4efe33307855f26beb33a8c37b
Natnael Abrham <natnael08@protonmain.com>
2020-04-04 01:11:08 +0300

    added posted_at property

--- server/api/models/post.model.js
+++ server/api/models/post.model.js
@@ -1,12 +1,13 @@
 const { Schema, model } = require("mongoose");
 
 const postSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: "user" },
+  user: { type: Schema.Types.ObjectId, ref: "user", required: true },
   type: { type: String, enum: ["text", "pic", "vid"] },
   text: String,
   pic: Array,
   vid: Array,
-  edited: { type: Boolean, default: false }
+  edited: { type: Boolean, default: false },
+  posted_at: { type: Date, default: Date.now },
 });
 
 module.exports = model("post", postSchema);
