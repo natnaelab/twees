e001c790f1d826858865f9916c124a4b282cd4b4
Natnael Abrham <natnael08@protonmain.com>
2020-04-04 23:20:09 +0300

    change pic & vid data type

--- server/api/models/post.model.js
+++ server/api/models/post.model.js
@@ -1,11 +1,17 @@
 const { Schema, model } = require("mongoose");
 
 const postSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: "user", required: true },
+  posted_by: { type: Schema.Types.ObjectId, ref: "user", required: true },
   type: { type: String, enum: ["text", "pic", "vid"] },
   text: String,
-  pic: Array,
-  vid: Array,
+  pic: {
+    caption: String,
+    url: String,
+  },
+  vid: {
+    caption: String,
+    url: String,
+  },
   edited: { type: Boolean, default: false },
   posted_at: { type: Date, default: Date.now },
 });
