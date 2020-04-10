bbb047dde27c8a3f92f9244887624410382e8f9e
Natnael Abrham <natnael08@protonmain.com>
2020-04-10 05:42:52 +0300

    removed pic & vid model properties, might be added in the future

--- server/api/models/post.model.js
+++ server/api/models/post.model.js
@@ -2,16 +2,9 @@ const { Schema, model } = require("mongoose");
 
 const postSchema = new Schema({
   posted_by: { type: Schema.Types.ObjectId, ref: "user", required: true },
-  type: { type: String, enum: ["text", "pic", "vid"] },
-  text: String,
-  pic: {
-    caption: String,
-    url: String,
-  },
-  vid: {
-    caption: String,
-    url: String,
-  },
+  type: { type: String, enum: ["text", "yt"] },
+  post: String,
+  youtube: String,
   edited: { type: Boolean, default: false },
   posted_at: { type: Date, default: Date.now },
 });
