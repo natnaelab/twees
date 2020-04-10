dd8ff05870d88806564bcb1e2a1f8e2252e7942e
Natnael Abrham <natnael08@protonmain.com>
2020-04-10 06:37:43 +0300

    removed pic & vid post and added youtube link

--- server/api/controllers/get.post.js
+++ server/api/controllers/get.post.js
@@ -2,7 +2,7 @@ const Post = require("../models/post.model");
 
 module.exports = {
   getAll: (req, res) => {
-    Post.find({}, "_id pic vid text type edited posted_by posted_at")
+    Post.find({}, "_id youtube text type edited posted_by posted_at")
       .sort({ posted_at: -1 })
       .populate("posted_by", "_id username avatar")
       .exec()
@@ -15,7 +15,7 @@ module.exports = {
   },
   getByUser: (req, res) => {
     let posted_by = req.params.user;
-    Post.find({ posted_by }, "_id pic vid text type edited posted_by posted_at")
+    Post.find({ posted_by }, "_id youtube text type edited posted_by posted_at")
       .sort({ posted_at: -1 })
       .populate("posted_by", "_id username avatar")
       .exec()
@@ -28,7 +28,7 @@ module.exports = {
   },
   getById: (req, res) => {
     let _id = req.params.id;
-    Post.findById(_id, "_id pic vid text type edited posted_by posted_at")
+    Post.findById(_id, "_id youtube text type edited posted_by posted_at")
       .populate("posted_by", "_id username avatar")
       .exec()
       .then((post) => {
--- server/api/controllers/new.post.js
+++ server/api/controllers/new.post.js
@@ -1,85 +1,21 @@
 const { validatePostInput } = require("../validator/post.validator");
-const fs = require("fs");
 const Post = require("../models/post.model");
-const isEmpty = require("../utils/isEmpty");
-const cloudUpload = require("../config/cloudinary");
 
 module.exports = (req, res) => {
-  let post = {};
-  post.posted_by = req.user.id;
-  Object.assign(post, req.body);
+  let newPost = {};
+  newPost.posted_by = req.user.id;
+  Object.assign(newPost, req.body);
 
   const { errors, isValid } = validatePostInput(req.body);
 
-  if (isEmpty(post.text) && isEmpty(req.file))
-    res.status(400).json({ status: "failed", message: "empty post" });
+  if (!newPost.text && !newPost.youtube)
+    return res.status(400).json({ status: "error", message: "empty post" });
 
   if (!isValid) {
     return res.status(400).json(errors);
   }
 
-  if (!req.file && post.text) {
-    post.type = "text";
-    return new Post(post)
-      .save()
-      .then(() => {
-        res.json({ status: "success" });
-      })
-      .catch((err) => {
-        res.status(400).json({ status: "error", message: err.message });
-      });
-  } else if (req.file) {
-    let mimetype = req.file.mimetype;
-    if (mimetype.includes("image")) {
-      post.type = "pic";
-      let pic = {};
-      if (post.caption) {
-        pic.caption = post.caption;
-        delete post.caption;
-      }
-      cloudUpload(req.file.path, req.user.username, "image")
-        .then((resp) => {
-          fs.unlinkSync(req.file.path);
-          pic.url = resp.url;
-          post.pic = pic;
-          return new Post(post)
-            .save()
-            .then((_) => {
-              res.json({ status: "success" });
-            })
-            .catch((err) => {
-              res.status(400).json({ status: "error", message: err.message });
-            });
-        })
-        .catch((err) => {
-          if (fs.existsSync(req.file.path)) fs.unlinkSync(req.file.path);
-          res.status(400).json({ status: "error", message: err.message });
-        });
-    } else if (mimetype.includes("video")) {
-      post.type = "vid";
-      let vid = {};
-      if (post.caption) {
-        vid.caption = post.caption;
-        delete post.caption;
-      }
-      cloudUpload(req.file.path, req.user.username, "video")
-        .then((resp) => {
-          fs.unlinkSync(req.file.path);
-          vid.url = resp.url;
-          post.vid = vid;
-          return new Post(post)
-            .save()
-            .then((_) => {
-              res.json({ status: "success" });
-            })
-            .catch((err) => {
-              res.status(400).json({ status: "error", message: err.message });
-            });
-        })
-        .catch((err) => {
-          if (fs.existsSync(req.file.path)) fs.unlinkSync(req.file.path);
-          res.status(400).json({ status: "error", message: err.message });
-        });
-    }
-  }
+  return new Post(newPost).save().then((resp) => {
+    res.json({ status: "success" });
+  });
 };
