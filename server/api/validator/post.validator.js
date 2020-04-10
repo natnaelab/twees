6db7886a013bda87c72f1912c5089aa7525da5ca
Natnael Abrham <natnael08@protonmain.com>
2020-04-10 06:07:00 +0300

    removed caption validation check & added youtube link validator

--- server/api/validator/post.validator.js
+++ server/api/validator/post.validator.js
@@ -5,34 +5,24 @@ const isEmpty = require("../utils/isEmpty");
 module.exports = {
   /**
    * post validator
-   * post-body (text)
-   * captions (pic/vid)
+   * post (text)
    */
   validatePostInput: (data) => {
     let errors = {};
 
     if (data.text) data.text = !isEmpty(data.text) ? data.text : "";
-    if (data.pic)
-      data.pic.caption = !isEmpty(data.pic.caption) ? data.pic.caption : "";
-    if (data.vid)
-      data.vid.caption = !isEmpty(data.vid.caption) ? data.vid.caption : "";
+    if (data.yt) data.yt = !isEmpty(data.yt) ? data.yt : "";
 
     // check text chars length
-    data.text &&
-      !validator.isLength(data.text, {
-        max: 400,
-      }) &&
-      (errors.text = "you can't post more than 400 characters");
+    !validator.isLength(data.text, {
+      max: 400,
+    }) && (errors.text = "characters limit exceeds");
 
     // check if text field is empty
-    data.text &&
-      validator.isEmpty(data.text) &&
-      (errors.text = "text field is required");
+    validator.isEmpty(data.text) && (errors.text = "text field is required");
 
-    data.caption &&
-      !isEmpty(data.caption) &&
-      !validator.isLength(data.caption, { max: 200 }) &&
-      (errors.caption = "you can't use more than 200 characters");
+    let yt_rgx = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+    !data.yt.match(yt_rgx) && (errors.yt = "invalid youtube link");
 
     // return error if the form is invalid
     return {
