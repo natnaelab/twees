a424d2570f4736b61d3015a2d9d39eb643c3c96d
Natnael Abrham <natnael08@protonmain.com>
2020-04-10 06:08:37 +0300

    moved upload function to util folder

--- server/api/config/cloudinary.js
+++ server/api/config/cloudinary.js
@@ -1,28 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
-const {
-  cloudinary_cloud_name,
-  cloudinary_api_key,
-  cloudinary_api_secret
-} = process.env;
+cloudinary.config({
+  cloud_name: process.env.cloudinary_cloud_name,
+  api_key: process.env.cloudinary_api_key,
+  api_secret: process.env.cloudinary_api_secret,
+});
 
-module.exports = (path, username, fileType) => {
-  return new Promise((resolve, reject) => {
-    cloudinary.config({
-      cloud_name: cloudinary_cloud_name,
-      api_key: cloudinary_api_key,
-      api_secret: cloudinary_api_secret
-    });
-    cloudinary.uploader.upload(
-      path,
-      {
-        resource_type: "auto",
-        folder: `posts/${username}/${fileType}`
-      },
-      (err, res) => {
-        if (err) reject(err);
-        resolve(res);
-      }
-    );
-  });
-};
+module.exports = cloudinary;
