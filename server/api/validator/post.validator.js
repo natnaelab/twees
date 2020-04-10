1b953910b22d1967e7945e0576dbefd7a3dfe7bc
Natnael Abrham <natnael08@protonmain.com>
2020-04-10 06:25:07 +0300

    small fixes

--- server/api/validator/post.validator.js
+++ server/api/validator/post.validator.js
@@ -11,18 +11,24 @@ module.exports = {
     let errors = {};
 
     if (data.text) data.text = !isEmpty(data.text) ? data.text : "";
-    if (data.yt) data.yt = !isEmpty(data.yt) ? data.yt : "";
+    if (data.youtube) data.youtube = !isEmpty(data.youtube) ? data.youtube : "";
 
     // check text chars length
-    !validator.isLength(data.text, {
-      max: 400,
-    }) && (errors.text = "characters limit exceeds");
+    data.text &&
+      !validator.isLength(data.text, {
+        max: 400,
+      }) &&
+      (errors.text = "characters limit exceeds");
 
     // check if text field is empty
-    validator.isEmpty(data.text) && (errors.text = "text field is required");
+    data.text &&
+      validator.isEmpty(data.text) &&
+      (errors.text = "text field is required");
 
     let yt_rgx = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
-    !data.yt.match(yt_rgx) && (errors.yt = "invalid youtube link");
+    data.youtube &&
+      !data.youtube.match(yt_rgx) &&
+      (errors.youtube = "invalid youtube link");
 
     // return error if the form is invalid
     return {
