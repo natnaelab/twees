ddb6ad09acbcbe22e2b4ca4b34396024e88802f4
Natnael Abrham <natnael08@protonmain.com>
2020-04-10 06:38:44 +0300

    replace post with text

--- server/api/models/post.model.js
+++ server/api/models/post.model.js
@@ -3,7 +3,7 @@ const { Schema, model } = require("mongoose");
 const postSchema = new Schema({
   posted_by: { type: Schema.Types.ObjectId, ref: "user", required: true },
   type: { type: String, enum: ["text", "yt"] },
-  post: String,
+  text: String,
   youtube: String,
   edited: { type: Boolean, default: false },
   posted_at: { type: Date, default: Date.now },
